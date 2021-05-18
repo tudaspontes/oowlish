23a02514459e945b705494598d57652f9fd80cb3
Marcelo Anselmo de Souza Filho <marcelofilho@mpf.mp.br>
2021-05-18 12:46:47 -0300

    Implementando redux

---
+++ src/store/type.d.ts
@@ -0,0 +1,14 @@
+interface IArticle {
+  id: number;
+  title: string;
+  body: string;
+}
+
+type ArticleState = {
+  articles: IArticle[];
+};
+
+type ArticleAction = {
+  type: string;
+  article: IArticle;
+};
